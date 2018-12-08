b1bf84aceffce002e83a480d8970607a416f5a2b
cyrillemidingoyi <34443570+cyrillemidingoyi@users.noreply.github.com>
2018-12-08 01:57:41 +0100

    Update Penman.cs

--- src/cs/Penman.cs
+++ src/cs/Penman.cs
@@ -1,1 +1,3 @@
+
+
 evapoTranspirationPenman = evapoTranspirationPriestlyTaylor / Alpha + 1000 * ((rhoDensityAir * specificHeatCapacityAir * VPDair * conductance) / (lambdaV * (hslope + psychrometricConstant)));
